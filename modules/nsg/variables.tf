79bfe847be0bf73dcdd685db05c6e390f2527d20
sivadotblog <hello@siva.blog>
2022-07-09 14:56:42 -0400

    adding adb nsg

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,14 @@
+variable "rgname" {
+    type = string
+    description = "Name of resource group"
+}
+
+variable "location" {
+    type = string
+    description = "Azure location of resource group"
+}
+
+variable "nsg" {
+    type = string
+
+}
