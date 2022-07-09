40d8b84ab3a9c1d898e4585172bb40b8dbff0260
sivadotblog <hello@siva.blog>
2022-07-09 11:14:34 -0400

    create rg

--- variables.tf
+++ variables.tf
@@ -13,3 +13,13 @@ variable "az-client-id" {
 variable "az-client-secret" {
   type = string
 }
+
+variable "rgname" {
+    type = string
+    description = "Name of resource group"
+}
+
+variable "location" {
+    type = string
+    description = "Azure location of resource group"
+}
