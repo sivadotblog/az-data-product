3d815a5c4c9e0371496171f44049621fad3f684b
sivadotblog <hello@siva.blog>
2022-07-10 15:31:54 -0400

    updated

--- modules/adb/ws/output.tf
+++ modules/adb/ws/output.tf
@@ -2,3 +2,8 @@ output "workspace_url" {
     value = azurerm_databricks_workspace.adb.workspace_url
 }
 
+
+
+output "managed_resource_group_name" {
+    value = azurerm_databricks_workspace.adb.managed_resource_group_name
+}
--- provider.tf
+++ provider.tf
@@ -21,5 +21,14 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  # Configuration options
+  azure_auth = {
+    managed_resource_group = module.adb_dataproduct_ws.managed_resource_group_name
+    azure_region           = var.location
+    workspace_name         = var.adb_ws
+    resource_group         = var.rgname
+    client_id              = var.az-client-id
+    client_secret          = var.az-client-secret
+    tenant_id              = var.az-tenant-id
+    subscription_id        = var.az-subscription-id
+  }
 }
