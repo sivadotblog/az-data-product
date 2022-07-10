768c488e7d80bebfc17039454cf05952ddb8923e
sivadotblog <hello@siva.blog>
2022-07-09 23:33:07 -0400

    updated snet

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -3,4 +3,18 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.rgname
   virtual_network_name = var.vnet
   address_prefixes     = var.subnet_address_space
+
+    dynamic "delegation" {
+        for_each = each.value.service_delegation == "true" ? [1] : []
+        
+        content {
+            name = var.delegation_name
+
+            service_delegation {
+            name    = var.service_delegation_name
+            actions = var.actions
+            }        
+        }
+    
+    }
 }
