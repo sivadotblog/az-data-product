327fc9654940fbc7e99433901ca8d52e857d0c36
sivadotblog <hello@siva.blog>
2022-07-11 12:09:26 -0400

    remove ws

--- provider.tf
+++ provider.tf
@@ -20,13 +20,13 @@ provider "azurerm" {
   client_secret     = var.az-client-secret
 }
 
-data "databricks_current_user" "me" {
+/*data "databricks_current_user" "me" {
   depends_on = [module.adb_dataproduct_ws]
-}
+}*/
 
 provider "databricks" {
   # Configuration options
-  host = module.adb_dataproduct_ws.workspace_url
+  //host = module.adb_dataproduct_ws.workspace_url
      
 }
 
