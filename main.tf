4d9335cb02a780a4a1aa9df2d39d74632c58d72f
sivadotblog <hello@siva.blog>
2022-07-11 12:06:14 -0400

    delete ws

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ module "dp_datastore" {
   rgname              = var.rgname
 }
 
-module "adb_dataproduct_ws" {
+/*module "adb_dataproduct_ws" {
   source    = "./modules/adb/ws"
 
   adb_ws              = var.adb_ws
@@ -96,11 +96,11 @@ module "adb_dataproduct_ws" {
   nsg_public_subnet_association_id=module.adb_public_subnet_nsg_association.nsg_association_id
   nsg_private_subnet_association_id=module.adb_private_subnet_nsg_association.nsg_association_id
   storage_account_name = var.dp_datastore
-}
+}*/
 
-module "adb_dataproduct_ws_global_init_sh" {
+/*module "adb_dataproduct_ws_global_init_sh" {
   source    = "./modules/adb/global_init"
   script_path =var.adb_global_init_path
   global_init_name = "global_init"
 
-}
+}*/
