72be4bbaa3c2dfa086f7fbe6a907d5347ccb9a38
sivadotblog <hello@siva.blog>
2023-01-25 12:25:27 -0500

    create 2nd ws

--- ws2.tf
+++ ws2.tf
@@ -1,6 +1,6 @@
 locals {
-  ws1 = {
-    rgname                           = "data-product"
+  ws2 = {
+    rgname                           = "data-product-2"
     location                         = "eastus"
     adb_vnet                         = "adb-vnet"
     adb_vnet_address_space           = ["10.0.0.0/24"]
@@ -10,39 +10,39 @@ locals {
     adb_public_subnet_address_space  = ["10.0.0.64/26"]
     adb_nsg                          = "adb-nsg"
     adb_ws                           = "adb-dataproduct-ws"
-    dp_datastore                     = "srdpdatastore"
+    dp_datastore                     = "srdpdatastorews2"
     adb_global_init_path             = "./global_init/global_initv2.sh"
     kvname                           = "data-product-kv"
   }
 }
 
-module "resource_group" {
+module "resource_group_ws2" {
   source = "./modules/resource_group"
 
-  rgname   = local.ws1.rgname
-  location = local.ws1.location
+  rgname   = local.ws2.rgname
+  location = local.ws2.location
 }
 
-module "adb_vnet" {
+module "adb_vnet_ws2" {
   source = "./modules/vnet"
 
-  vnet               = local.ws1.adb_vnet
-  vnet_address_space = local.ws1.adb_vnet_address_space
-  location           = local.ws1.location
-  rgname             = local.ws1.rgname
+  vnet               = local.ws2.adb_vnet
+  vnet_address_space = local.ws2.adb_vnet_address_space
+  location           = local.ws2.location
+  rgname             = local.ws2.rgname
   depends_on = [
-    module.resource_group
+    module.resource_group_ws2
   ]
 }
 
-module "adb_private_subnet" {
+module "adb_private_subnet_ws2" {
   source = "./modules/subnet"
 
-  subnet               = local.ws1.adb_private_subnet
-  subnet_address_space = local.ws1.adb_private_subnet_address_space
-  location             = local.ws1.location
-  rgname               = local.ws1.rgname
-  vnet                 = local.ws1.adb_vnet
+  subnet               = local.ws2.adb_private_subnet
+  subnet_address_space = local.ws2.adb_private_subnet_address_space
+  location             = local.ws2.location
+  rgname               = local.ws2.rgname
+  vnet                 = local.ws2.adb_vnet
 
   delegation_name = "adb-databricks-del"
   actions = [
@@ -53,20 +53,20 @@ module "adb_private_subnet" {
   service_delegation_name = "Microsoft.Databricks/workspaces"
 
   depends_on = [
-    module.resource_group,
-    module.adb_vnet
+    module.resource_group_ws2,
+    module.adb_vnet_ws2
 
   ]
 }
 
-module "adb_public_subnet" {
+module "adb_public_subnet_ws2" {
   source = "./modules/subnet"
 
-  subnet               = local.ws1.adb_public_subnet
-  subnet_address_space = local.ws1.adb_public_subnet_address_space
-  location             = local.ws1.location
-  rgname               = local.ws1.rgname
-  vnet                 = local.ws1.adb_vnet
+  subnet               = local.ws2.adb_public_subnet
+  subnet_address_space = local.ws2.adb_public_subnet_address_space
+  location             = local.ws2.location
+  rgname               = local.ws2.rgname
+  vnet                 = local.ws2.adb_vnet
   delegation_name      = "adb-databricks-del"
   actions = [
     "Microsoft.Network/virtualNetworks/subnets/join/action",
@@ -76,71 +76,71 @@ module "adb_public_subnet" {
   service_delegation_name = "Microsoft.Databricks/workspaces"
 
   depends_on = [
-    module.resource_group,
-    module.adb_vnet
+    module.resource_group_ws2,
+    module.adb_vnet_ws2
   ]
 }
 
-module "adb_nsg" {
+module "adb_nsg_ws2" {
   source = "./modules/nsg"
 
-  nsg      = local.ws1.adb_nsg
-  location = local.ws1.location
-  rgname   = local.ws1.rgname
+  nsg      = local.ws2.adb_nsg
+  location = local.ws2.location
+  rgname   = local.ws2.rgname
   depends_on = [
-    module.resource_group
+    module.resource_group_ws2
   ]
 }
 
-module "adb_private_subnet_nsg_association" {
+module "adb_private_subnet_nsg_association_ws2" {
   source = "./modules/nsg_association"
 
   nsg_id    = module.adb_nsg.nsg_id
   subnet_id = module.adb_private_subnet.subnet_id
   depends_on = [
-    module.adb_private_subnet
+    module.adb_private_subnet_ws2
   ]
 
 }
 
-module "adb_public_subnet_nsg_association" {
+module "adb_public_subnet_nsg_association_ws2" {
   source = "./modules/nsg_association"
 
   nsg_id    = module.adb_nsg.nsg_id
   subnet_id = module.adb_public_subnet.subnet_id
   depends_on = [
-    module.adb_public_subnet
+    module.adb_public_subnet_ws2
   ]
 
 }
 
-module "dp_datastore" {
+module "dp_datastore_ws2" {
   source = "./modules/storage"
 
-  storage_account_name = local.ws1.dp_datastore
-  location             = local.ws1.location
-  rgname               = local.ws1.rgname
+  storage_account_name = local.ws2.dp_datastore
+  location             = local.ws2.location
+  rgname               = local.ws2.rgname
   depends_on = [
-    module.resource_group,
+    module.resource_group_ws2,
   ]
 }
 
-module "adb_dataproduct_ws" {
+module "adb_dataproduct_ws2" {
   source = "./modules/adb/ws"
 
-  adb_ws                            = local.ws1.adb_ws
-  rgname                            = local.ws1.rgname
-  location                          = local.ws1.location
-  public_subnet_name                = local.ws1.adb_public_subnet
-  private_subnet_name               = local.ws1.adb_private_subnet
+  adb_ws                            = local.ws2.adb_ws
+  rgname                            = local.ws2.rgname
+  location                          = local.ws2.location
+  public_subnet_name                = local.ws2.adb_public_subnet
+  private_subnet_name               = local.ws2.adb_private_subnet
   vnet_id                           = module.adb_vnet.vnet_id
   nsg_public_subnet_association_id  = module.adb_public_subnet_nsg_association.nsg_association_id
   nsg_private_subnet_association_id = module.adb_private_subnet_nsg_association.nsg_association_id
-  storage_account_name              = substr(replace(join("", [local.ws1.dp_datastore, local.ws1.adb_ws]), "-", ""), 1, 24)
+  storage_account_name              = substr(replace(join("", [local.ws2.dp_datastore, local.ws2.adb_ws]), "-", ""), 1, 24)
   depends_on = [
-    module.adb_private_subnet_nsg_association,
-    module.adb_public_subnet_nsg_association,
-    module.dp_datastore,
+    module.adb_private_subnet_nsg_association_ws2,
+    module.adb_public_subnet_nsg_association_ws2,
+    module.dp_datastore_ws2,
 
   ]
 }
@@ -149,7 +149,7 @@ module "adb_dataproduct_ws" {
 /*
 module "adb_dataproduct_ws_global_init_sh" {
   source           = "./modules/adb/global_init"
-  script_path      = local.ws1.adb_global_init_path
+  script_path      = local.ws2.adb_global_init_path
   global_init_name = "global_init"
 
 }
