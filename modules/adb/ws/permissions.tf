052a478dbcafdfabd615d50493d7f53c47aef803
sivadotblog <hello@siva.blog>
2023-01-25 14:11:38 -0500

    added spn admin

--- modules/adb/ws/permissions.tf
+++ modules/adb/ws/permissions.tf
@@ -12,7 +12,12 @@ data "databricks_group" "admins" {
   display_name = "admins"
 }
 
+
+resource "databricks_service_principal" "sp" {
+  application_id = "2fbbfd39-22ed-439c-bf79-d6a92318ce35"
+}
+
 resource "databricks_group_member" "i-am-admin" {
   group_id  = data.databricks_group.admins.id
-  member_id = databricks_user.me.id
+  member_id = databricks_service_principal.sp.id
 }
