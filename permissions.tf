212e9fd31e55d811cf6b71dfec8d1ca32d752f18
sivadotblog <hello@siva.blog>
2023-01-25 10:49:02 -0500

    added permissions

---
+++ permissions.tf
@@ -0,0 +1,12 @@
+data "databricks_group" "admins" {
+  display_name = "admins"
+}
+
+resource "databricks_user" "me" {
+  user_name = "sivanandha@live.com"
+}
+
+resource "databricks_group_member" "i-am-admin" {
+  group_id  = data.databricks_group.admins.id
+  member_id = databricks_user.me.id
+}
