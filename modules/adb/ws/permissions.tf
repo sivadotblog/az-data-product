3d74a8816ae1fba7906063d89a5276a25a9b7c89
sivadotblog <hello@siva.blog>
2023-01-25 13:32:16 -0500

    add spn and user

--- modules/adb/ws/permissions.tf
+++ modules/adb/ws/permissions.tf
@@ -10,13 +10,3 @@ resource "databricks_group_member" "i-am-admin" {
   group_id  = data.databricks_group.admins.id
   member_id = databricks_user.me.id
 }
-
-
-
-resource "databricks_service_principal" "sp" {
-  application_id = "2fbbfd39-22ed-439c-bf79-d6a92318ce35"
-}
-resource "databricks_group_member" "i-am-admin-spn" {
-  group_id  = data.databricks_group.admins.id
-  member_id = databricks_service_principal.sp.id
-}
